ce36ff094318512022168c397dfe2901ef2b4bbe
aterry17 <aterry1@byu.edu>
2025-09-17 17:49:33 -0600

    (p0) BishopMovesCalculator fix pieceMoves -- all BishopMoveTests currently passing

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -20,20 +20,19 @@ public class BishopMovesCalculator implements PieceMovesCalculator{ // extends m
             var curr_position = new ChessPosition(startPosition.getRow() + direction[0], startPosition.getColumn() + direction[1]);
             while(is_position_on_board(curr_position) && is_position_empty(board, curr_position)){
                 list_of_moves.add(new ChessMove(startPosition, curr_position, null));
-                curr_position = new ChessPosition(startPosition.getRow() + direction[0], startPosition.getColumn() + direction[1]);
+                curr_position = new ChessPosition(curr_position.getRow() + direction[0], curr_position.getColumn() + direction[1]);
             }
             // since the while loop broke, either we're off the board or we ran into another player
             // make the capture enemy move here
-            var potential_enemy_position = new ChessPosition(startPosition.getRow() + direction[0], startPosition.getColumn() + direction[1]);
+            var potential_enemy_position = curr_position;
             // make sure it's actually on the board
-            if (!is_position_on_board(potential_enemy_position)) continue; // I think there's a serious issue here with the continue saying it's not doing anything
+            if (!is_position_on_board(potential_enemy_position)) continue; // continue is unecessary because there's no more code outside of the is / else if / else block -- but as soon as you add code beneath the block, the continue becomes very necessary
             // check the color
-            else if(team_color == board.getPiece(potential_enemy_position).getTeamColor()) continue; // same goes for this continue
+            else if(team_color == board.getPiece(potential_enemy_position).getTeamColor()) continue;
             // assuming that everything is doing what I think it's doing, we should know herre that we
             else {
                 list_of_moves.add(new ChessMove(startPosition, potential_enemy_position, null));
             }
-
         }
 
         return list_of_moves;
@@ -58,37 +57,38 @@ public class BishopMovesCalculator implements PieceMovesCalculator{ // extends m
 
     public boolean is_position_on_board(ChessPosition currPosition){
         // find if position coordinates are on the board
-        return !(currPosition.getRow() < 1 || currPosition.getRow() > 8 || currPosition.getColumn() < 1 || currPosition.getColumn() > 8);
+        return ((currPosition.getRow() >= 1) && (currPosition.getRow() <= 8) && (currPosition.getColumn() >=1) && (currPosition.getColumn() <= 8));
     }
     public boolean is_position_empty(ChessBoard board, ChessPosition currPosition){
          // find if someone else is in the position
-        if (! is_position_on_board(currPosition)) {
+        if (!is_position_on_board(currPosition)) {
             throw new RuntimeException("Position is off board");
          }
         return (board.getPiece(currPosition) == null);
      }
 
-     public ChessPosition capture_enemy_move(ChessBoard board, ChessGame.TeamColor team_color, int curr_row, int curr_col) { // maybe put this in piecemovescalc (but implement it here)
-        var final_position = new ChessPosition(curr_row, curr_col);
-        var next_up_position = new ChessPosition(curr_row+1, curr_col+1);
-
-        boolean pos_on_board = is_position_on_board(next_up_position);
-
-         boolean piece_in_pos = (board.getPiece(next_up_position) != null);
-         boolean enemy_piece = false;
-
-         if (piece_in_pos) { // here to avoid NullPointerException
-             enemy_piece = (team_color != board.getPiece(next_up_position).getTeamColor());
-         }
-
-         if (pos_on_board && piece_in_pos && enemy_piece){
-             return next_up_position;
-         }
-         else {
-             return final_position;
-         }
-
-     }
+//
+//     public ChessPosition capture_enemy_move(ChessBoard board, ChessGame.TeamColor team_color, int curr_row, int curr_col) { // maybe put this in piecemovescalc (but implement it here)
+//        var final_position = new ChessPosition(curr_row, curr_col);
+//        var next_up_position = new ChessPosition(curr_row+1, curr_col+1);
+//
+//        boolean pos_on_board = is_position_on_board(next_up_position);
+//
+//         boolean piece_in_pos = (board.getPiece(next_up_position) != null);
+//         boolean enemy_piece = false;
+//
+//         if (piece_in_pos) { // here to avoid NullPointerException
+//             enemy_piece = (team_color != board.getPiece(next_up_position).getTeamColor());
+//         }
+//
+//         if (pos_on_board && piece_in_pos && enemy_piece){
+//             return next_up_position;
+//         }
+//         else {
+//             return final_position;
+//         }
+//
+//     }
 
 
 }
