a942e4790382181cdcfcc84dbe34587b6f935a82
aterry17 <aterry1@byu.edu>
2025-09-18 13:59:27 -0600

    (p0) PawnMovesCalculator implement pieceMoves forward moves

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -9,35 +9,51 @@ public class PawnMovesCalculator implements PieceMovesCalculator {
 
         var team_color = board.getPiece(startPosition).getTeamColor();
 
-        // if it is the first time it is being moved, may move forward two
-        if ((team_color == ChessGame.TeamColor.WHITE) && (startPosition.getRow() == 2)) {
-            list_of_moves.add(new ChessMove(startPosition, new ChessPosition(4, startPosition.getColumn()), null)); // is the promotionPiece parameter supposed to be null here?
+        int[][] white_enemy_capture_directions = {{1,1}, {1,-1}};
+        int[][] black_enemy_capture_directions = {{-1, -1}, {-1,1}};
+
+        var next_up_position_white = new ChessPosition(startPosition.getRow()+1, startPosition.getColumn());
+        var two_up_position_white = new ChessPosition(4, startPosition.getColumn());
+        var next_up_position_black = new ChessPosition(startPosition.getRow()-1, startPosition.getColumn());
+        var two_up_position_black = new ChessPosition(5, startPosition.getColumn());
+
+        if ((team_color == ChessGame.TeamColor.WHITE) && is_position_on_board(next_up_position_white) && is_position_empty(board, next_up_position_white)) {
+            list_of_moves.add(new ChessMove(startPosition, next_up_position_white, null));
+            if(is_position_on_board(two_up_position_white) && is_position_empty(board, next_up_position_white) && is_position_empty(board, two_up_position_white)){
+                list_of_moves.add(new ChessMove(startPosition, two_up_position_white, null));
+            }
+
+            // add in enemy capture
         }
-        if ((team_color == ChessGame.TeamColor.BLACK) && (startPosition.getRow() == 7)) {
-            list_of_moves.add(new ChessMove(startPosition, new ChessPosition(5, startPosition.getColumn()), null));
+
+        else if ((team_color == ChessGame.TeamColor.BLACK) && is_position_on_board(next_up_position_black) && is_position_empty(board, next_up_position_black)) {
+            list_of_moves.add(new ChessMove(startPosition, next_up_position_black, null));
+            if(is_position_on_board(two_up_position_black) && is_position_empty(board, next_up_position_black) && is_position_empty(board, two_up_position_black)){
+                list_of_moves.add(new ChessMove(startPosition, two_up_position_black, null));
+            }
+
+            // add in enemy capture
         }
-        // move forward 1 square (just forward not to the other side)
-        if (team_color == ChessGame.TeamColor.WHITE) {
-            list_of_moves.add(new ChessMove(startPosition, new ChessPosition(3, startPosition.getColumn()), null));
-        }
-        if (team_color == ChessGame.TeamColor.BLACK) {
-            list_of_moves.add(new ChessMove(startPosition, new ChessPosition(6, startPosition.getColumn()), null));
-        }
+
+
+
+
         // enemy capture: diag_up_right or diag_up_left
         // need to check if diag_up_ positions have something in them before trying to use .getPiece --> right now it is returning null and that throws an exception
 
-        var diag_up_right_pos = new ChessPosition(startPosition.getRow() + 1, startPosition.getColumn() + 1);
-        var diag_up_left_pos = new ChessPosition(startPosition.getRow() + 1, startPosition.getColumn() - 1);
-        if ((board.getPiece(diag_up_right_pos) != null) && (team_color != board.getPiece(diag_up_right_pos).getTeamColor())) {
-            list_of_moves.add(new ChessMove(startPosition, diag_up_right_pos, null));
-        }
-        if ((board.getPiece(diag_up_left_pos) != null) && (team_color != board.getPiece(diag_up_left_pos).getTeamColor())) {
-            list_of_moves.add(new ChessMove(startPosition, diag_up_left_pos, null));
-        }
-        // promotion: when white pawn reaches row 8 or black pawn reaches row 1, replaced by player's choice of rook, knight, bishop, queen
-        // don't know how to do this one yet, so I'll come back here later to fix it
-        // probably add the move, but inside the move the promotionPiece shouldn't be null -- it should be the piece type to put in -- how do we take into account the player's choice of piece?
+//        var diag_up_right_pos = new ChessPosition(startPosition.getRow() + 1, startPosition.getColumn() + 1);
+//        var diag_up_left_pos = new ChessPosition(startPosition.getRow() + 1, startPosition.getColumn() - 1);
+//        if ((board.getPiece(diag_up_right_pos) != null) && (team_color != board.getPiece(diag_up_right_pos).getTeamColor())) {
+//            list_of_moves.add(new ChessMove(startPosition, diag_up_right_pos, null));
+//        }
+//        if ((board.getPiece(diag_up_left_pos) != null) && (team_color != board.getPiece(diag_up_left_pos).getTeamColor())) {
+//            list_of_moves.add(new ChessMove(startPosition, diag_up_left_pos, null));
+//        }
+//        // promotion: when white pawn reaches row 8 or black pawn reaches row 1, replaced by player's choice of rook, knight, bishop, queen
+//        // don't know how to do this one yet, so I'll come back here later to fix it
+//        // probably add the move, but inside the move the promotionPiece shouldn't be null -- it should be the piece type to put in -- how do we take into account the player's choice of piece?
 
         return list_of_moves;
     }
+
 }
