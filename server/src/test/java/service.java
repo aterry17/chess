b02bcf6d03ce540e8bab44f9158a17428fe59918
aterry17 <aterry1@byu.edu>
2025-10-29 13:02:09 -0600

    (p3) implement service.java registerPositiveTest

---
+++ server/src/test/java/service.java
@@ -0,0 +1,29 @@
+import dataaccess.DataAccessException;
+import dataaccess.MemUserDAO;
+import service.Service;
+import model.RegisterRequest;
+import model.RegisterResult;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class service {
+
+    @Test
+    public void registerPositiveTest(){
+        Service service = new Service(new MemUserDAO());
+        // check to see that the register result properly sent with the correct username
+        try {
+            var result = service.register(new RegisterRequest("user1", "pass1", "email1"));
+            assertEquals(result.username(), "user1");
+        } catch (DataAccessException e){
+            fail("service.register threw an unexpected DataAccessException");
+        }
+    }
+
+    @Test
+    public void registerNegativeTest(){
+
+    }
+
+
+}
