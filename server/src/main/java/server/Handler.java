975aaf15e458e517aa37463b2e611a2c3d7b2555
aterry17 <aterry1@byu.edu>
2026-03-02 16:55:44 -0700

    (p3-S2) code quality

--- server/src/main/java/server/Handler.java
+++ server/src/main/java/server/Handler.java
@@ -7,26 +7,8 @@ import io.javalin.http.Context;
 import model.*;
 import service.Service;
 
-import java.util.HashSet;
-import java.util.Map;
 
 public class Handler {
-
-    /// example from class slides:
-//    LoginRequest request = (LoginRequest)gson.fromJson(reqData, LoginRequest.class);
-//    LoginService service = new LoginService();
-//    LoginResult result = service.login(request);
-//    return gson.toJson(result);
-
-    /// example of what a handler needs to do:
-    // validate the auth token
-    // deserialize JSON request to java request object
-    // call service class & pass to it the java request object
-    // receive response object from service class
-    // serialize java response object to JSON
-    // send http response back to client
-
-    //
 
     public void handleRegisterRequest(Service service, Context context) throws DataAccessException {
         Gson gson = new Gson();
